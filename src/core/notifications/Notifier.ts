60b0598f9c88be3a1a1cc8c583a77c0aa2f2046d
Przemysław P <toyerek@gmail.com>
2018-03-27 12:24:44 +0200

    Updated Notifier

--- src/core/notifications/Notifier.ts
+++ src/core/notifications/Notifier.ts
@@ -1,39 +1,35 @@
 import { NotificationType } from "../enums/NotificationType";
 import Browser from "../Browser";
+
 export default class Notifier {
 
-<<<<<<< Updated upstream
     browser: Browser = new Browser("package://toast/index.html", true, false, false);
-    type: NTypes = NTypes.INFO;
+    type: NotificationType = NotificationType.Info;
     data: Array<string> = [];
 
-    public start(){
+    public start() {
         var b = this.browser;
         b.execute(`parseData('${JSON.stringify(this.data)}')`);
     }
 
-    public init(type: NTypes,title: string,message: string){
+    // Blagam nie uzywajmy w tym projekcie WIELKICH LITER 
+    public init(type: NotificationType, title: string, message: string) {
         this.data = [];
         let background = "purple";
         let messageColor = "white";
-        switch(type){
-            case NTypes.INFO:
+        switch (type) {
+            case NotificationType.Info:
                 background = "grey"
                 break;
-            case NTypes.WARNING:
+            case NotificationType.Warning:
                 background = "pink"
                 break;
-            case NTypes.ERROR:
+            case NotificationType.Error:
                 background = "red";
                 break;
         }
         this.type = type;
-        this.data = [title,message,background,messageColor];
-=======
-    public init(ntype: NotificationType, value: string){
-        var browser: Browser = new Browser("package://toast/index.html", true, false, false);
-        browser.execute('showToast(${JSON.stringify([0,"Testowa wiadomosc"])})');
->>>>>>> Stashed changes
+        this.data = [title, message, background, messageColor];
     }
 
 
