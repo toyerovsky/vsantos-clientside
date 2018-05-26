ccb53b08ab09cf3dd2ffc8ddca691b9cd6929619
k1k9 <k1k9@protonmail.com>
2018-05-26 23:30:29 +0200

    added character creator components

--- spa-cef/src/app/app.module.ts
+++ spa-cef/src/app/app.module.ts
@@ -1,19 +1,24 @@
 // modules
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
-
-// components
 import { AppComponent } from './app.component';
-import { LoginPanelComponent } from "./view/login-panel/login-panel.component";
-import { CharacterSelectorComponent } from "./view/character-selector/character-selector.component";
-
-// services
 import { AccountService } from './service/account.service';
 import { CharacterService } from './service/character.service';
+import { CharacterSelectorComponent } from './view/character-selector/character-selector.component';
+import { LoginPanelComponent } from './view/login-panel/login-panel.component';
+import { CharacterCreatorComponent } from './view/character-creator/character-creator.component';
+import { CharacterCreatorMenuComponent } from './view/character-creator/elements/character-creator-menu/character-creator-menu.component';
+import { CharacterCreatorAppearanceComponent } from './view/character-creator/elements/character-creator-appearance/character-creator-appearance.component';
+import { CharacterCreatorAppearanceRootsComponent } from './view/character-creator/elements/character-creator-appearance/character-creator-appearance-roots/character-creator-appearance-roots.component';
+import { CharacterCreatorFeaturesComponent } from './view/character-creator/elements/character-creator-appearance/character-creator-features/character-creator-features.component';
+import { CharacterCreatorMoreAppearanceComponent } from './view/character-creator/elements/character-creator-appearance/character-creator-more-appearance/character-creator-more-appearance.component';
 
+// components
+// services
 @NgModule({
   imports: [
 
@@ -25,7 +30,13 @@ import { CharacterService } from './service/character.service';
   declarations: [
     AppComponent,
     LoginPanelComponent,
-    CharacterSelectorComponent
+    CharacterSelectorComponent,
+    CharacterCreatorComponent,
+    CharacterCreatorMenuComponent,
+    CharacterCreatorAppearanceComponent,
+    CharacterCreatorAppearanceRootsComponent,
+    CharacterCreatorFeaturesComponent,
+    CharacterCreatorMoreAppearanceComponent
   ],
   providers: [AccountService, CharacterService],
   bootstrap: [AppComponent],
