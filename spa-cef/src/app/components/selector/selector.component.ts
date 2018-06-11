967fd0ec0f592f8b431b938ce5d9e4edbc76d707
toyerovsky <przemyslaw.postrach@hotmail.com>
2018-06-11 15:41:40 +0200

    Add change event to selector

--- spa-cef/src/app/components/selector/selector.component.ts
+++ spa-cef/src/app/components/selector/selector.component.ts
@@ -11,20 +11,25 @@ export class SelectorComponent implements OnInit {
   private _currentIndex: number;
 
   @Input() public elements: ISelectorElement[];
-  // @Output() change: EventEmitter<ISelectorElement> = new EventEmitter();
+  @Output() public change: EventEmitter<ISelectorElement> = new EventEmitter();
 
   ngOnInit() {
-    alert(JSON.stringify(this.elements))
     this._currentElement = this.elements[0].displayName || '';
   }
 
   next() {
-    if (this.elements.length > this._currentIndex)
-      this._currentElement = this.elements[this._currentIndex++].displayName;
+    if (this.elements.length > this._currentIndex) {
+      let element: ISelectorElement = this.elements[++this._currentIndex];
+      this._currentElement = element.displayName;
+      this.change.emit(element);
+    }
   }
 
   prev() {
-    if (this.elements.length > 0)
-      this._currentElement = this.elements[this._currentIndex--].displayName;
+    if (this.elements.length > 0) {
+      let element: ISelectorElement = this.elements[--this._currentIndex];
+      this._currentElement = element.displayName;
+      this.change.emit(element);
+    }
   }
 }
