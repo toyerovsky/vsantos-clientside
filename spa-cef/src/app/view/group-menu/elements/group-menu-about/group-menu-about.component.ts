04f3938d08b7903ae19c0b4bf5689e60e382439b
Cherro <kaliszewski.dominik@gmail.com>
2018-06-30 22:43:08 +0200

    Add new notification and validation in some Components

--- spa-cef/src/app/view/group-menu/elements/group-menu-about/group-menu-about.component.ts
+++ spa-cef/src/app/view/group-menu/elements/group-menu-about/group-menu-about.component.ts
@@ -1,5 +1,6 @@
 import { Location } from '@angular/common';
 import { Component, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-group-menu-about',
@@ -7,12 +8,20 @@ import { Component, OnInit, Output } from '@angular/core';
 })
 export class GroupMenuAboutComponent implements OnInit {
 
-  constructor(private location: Location) { }
+  constructor(
+    private location: Location,
+    private _notification: ToastrService
+  ) { }
 
   ngOnInit() {
   }
 
   private previousPage(){
     this.location.back();
+  }
+
+  kickEmployee(): void{
+    //Check player permission in group
+    this._notification.info("Pomyślnie wyrzucono gracza XXX z grupy","GRUPA");
   }
 }
--- spa-cef/src/app/view/group-menu/elements/group-menu-summary/group-menu-summary.component.ts
+++ spa-cef/src/app/view/group-menu/elements/group-menu-summary/group-menu-summary.component.ts
@@ -1,14 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-group-menu-summary',
   templateUrl: './group-menu-summary.component.html'
 })
 export class GroupMenuSummaryComponent implements OnInit {
-
-  constructor() { }
+  public isOnDuty: boolean;
+  constructor(
+    private _notification: ToastrService
+  ) { }
 
   ngOnInit() {
+    //TEST
+    this.isOnDuty = false;
   }
+
+  duty(){
+    if(!this.isOnDuty){
+      this._notification.info("Wszedłeś właśnie na służbe!","Grupa");
+    }
+    else{
+      this._notification.info("Zszedłeś właśnie ze służby!","Grupa");
+    }
+    this.isOnDuty = !this.isOnDuty;
+
+  }
+
+
+
 
 }
